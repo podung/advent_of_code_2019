106c6acc9b0a65e61adbb7cdc20161a117d21389
Joe DePung <joe.depung@gmail.com>
2019-12-01 14:31:45 -0600

    project structure for advent of code 2019

---
+++ lib/day_01.ex
@@ -0,0 +1,5 @@
+defmodule Day01 do
+  def hello do
+    :world
+  end
+end
---
+++ test/day_01_test.exs
@@ -0,0 +1,7 @@
+defmodule Day01Test do
+  use ExUnit.Case
+
+  test "greets the world" do
+    assert Day01.hello() == :world
+  end
+end
