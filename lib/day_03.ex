62bfc7ca2aa8fd96d44c5d57a8f1e38109a2fad7
Joe DePung <joe.depung@gmail.com>
2019-12-03 21:28:39 -0600

    Day03, part 1

--- lib/day_03.ex
+++ lib/day_03.ex
@@ -1,20 +1,27 @@
 defmodule Day03 do
   def distance_to_closest_intersection(first,second) do
 
-    parse_wire(first)
+    first_points = parse_wire(first)
+    second_points = parse_wire(second)
+
+    first_points
+    |> MapSet.intersection(second_points)
+    |> MapSet.delete({0,0})
+    |> Enum.map(&calculate_manhatten_distance/1)
+    |> Enum.min
   end
 
   defp parse_wire(wire) do
     wire
     |> String.split(",")
-    |> Enum.map(&parse_direction/1)
+    |> Enum.map(&parse_directions/1)
     |> Enum.reduce([{0,0}], fn section, acc ->
       acc ++ plot_points(section, acc |> List.last)
     end)
-    |> Enum.map(&IO.inspect/1)
+    |> MapSet.new
   end
 
-  defp parse_direction(<< dir :: utf8, length :: bitstring>>),
+  defp parse_directions(<< dir :: utf8, length :: bitstring>>),
     do: { List.to_string([dir]), String.to_integer(length) }
 
   defp plot_points({ "R", length }, { x, y }) do
@@ -32,4 +39,6 @@ defmodule Day03 do
   defp plot_points({ "D", length }, { x, y }) do
     for x <- [x], y <- y-1..y-length, do: { x, y }
   end
+
+  defp calculate_manhatten_distance({ x, y }), do: abs(x) + abs(y)
 end
--- test/day_03_test.exs
+++ test/day_03_test.exs
@@ -1,8 +1,28 @@
 defmodule Day03Test do
   use ExUnit.Case
 
+  import Aoc2019Utils
+
   test "calculates sample closest distance correctly" do
-    assert Day03.distance_to_closest_intersection("R8,U5,L5,D3","U7,R6,D4,L4") == 156
-    #assert Day03.distance_to_closest_intersection("R8,U5,L5,D3","U7,R6,D4,L4") |> Enum.count == 5
+    assert Day03.distance_to_closest_intersection(
+      "R8,U5,L5,D3",
+      "U7,R6,D4,L4") == 6
+
+    assert Day03.distance_to_closest_intersection(
+      "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+      "U62,R66,U55,R34,D71,R55,D58,R83") == 159
+
+
+    assert Day03.distance_to_closest_intersection(
+      "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+      "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") == 135
+  end
+
+  test "calculates input data closest distance correctly" do
+    [wire1, wire2] = "fixtures/day03_fixture.txt"
+      |> File.read!
+      |> split_lines
+
+    assert Day03.distance_to_closest_intersection(wire1, wire2) == 227
   end
 end
