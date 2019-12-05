952cc31d8d6dfcc30d5509a31ae787fe95191e6b
Joe DePung <joe.depung@gmail.com>
2019-12-04 22:47:09 -0600

    Day 03, part 2

--- lib/day_03.ex
+++ lib/day_03.ex
@@ -1,13 +1,28 @@
 defmodule Day03 do
   def distance_to_closest_intersection(first,second) do
 
-    first_points = parse_wire(first)
-    second_points = parse_wire(second)
+    first_points = parse_wire(first) |> MapSet.new
+    second_points = parse_wire(second) |> MapSet.new
 
     first_points
     |> MapSet.intersection(second_points)
     |> MapSet.delete({0,0})
     |> Enum.map(&calculate_manhatten_distance/1)
+    |> Enum.min
+  end
+
+  def fewest_steps_to_closest_intersection(first,second) do
+    first_points = parse_wire(first)
+    second_points = parse_wire(second)
+
+    intersections = MapSet.new(first_points)
+                      |> MapSet.intersection(MapSet.new(second_points))
+                      |> MapSet.delete({0,0})
+
+    intersections
+    |> Enum.map(fn intersection ->
+        steps_to(first_points, intersection) + steps_to(second_points, intersection)
+      end)
     |> Enum.min
   end
 
@@ -18,7 +33,6 @@ defmodule Day03 do
     |> Enum.reduce([{0,0}], fn section, acc ->
       acc ++ plot_points(section, acc |> List.last)
     end)
-    |> MapSet.new
   end
 
   defp parse_directions(<< dir :: utf8, length :: bitstring>>),
@@ -38,6 +52,10 @@ defmodule Day03 do
 
   defp plot_points({ "D", length }, { x, y }) do
     for x <- [x], y <- y-1..y-length, do: { x, y }
+  end
+
+  defp steps_to(points, intersection) do
+    Enum.find_index(points, &(&1 == intersection))
   end
 
   defp calculate_manhatten_distance({ x, y }), do: abs(x) + abs(y)
--- test/day_03_test.exs
+++ test/day_03_test.exs
@@ -25,4 +25,22 @@ defmodule Day03Test do
 
     assert Day03.distance_to_closest_intersection(wire1, wire2) == 227
   end
+
+  test "calculates sample data fewest steps correctly" do
+    assert Day03.fewest_steps_to_closest_intersection(
+      "R75,D30,R83,U83,L12,D49,R71,U7,L72",
+      "U62,R66,U55,R34,D71,R55,D58,R83") == 610
+
+    assert Day03.fewest_steps_to_closest_intersection(
+      "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51",
+      "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7") == 410
+  end
+
+  test "calculates input data fewest steps correctly" do
+    [wire1, wire2] = "fixtures/day03_fixture.txt"
+      |> File.read!
+      |> split_lines
+
+    assert Day03.fewest_steps_to_closest_intersection(wire1, wire2) == 20286
+  end
 end
